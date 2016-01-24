f42b0045baaf180a5d910b7d49aa1d69a376c969
Arturo Vergara <hello@arturovm.me>
2016-01-24 12:38:56 -0600

    tabla de peers ahora filtrada

--- src/networking/peers.go
+++ src/networking/peers.go
@@ -12,10 +12,22 @@ func LookupPeers() <-chan []string {
 	entriesCh := make(chan *mdns.ServiceEntry, 4)
 	resChan := make(chan []string, 1)
 	go func(ch chan<- []string) {
-		var entries []string
+		var entries []string             // peers
+		addrs, _ := net.InterfaceAddrs() // interfaces locales
 		for entry := range entriesCh {
-			entries = append(entries, net.JoinHostPort(entry.AddrV4.String(),
-				fmt.Sprintf("%d", entry.Port)))
+			addr := entry.AddrV4 // dirección de host encontrado
+			skip := false        // saltar o no esta dirección
+			// buscar si peer encontrado es máquina propia
+			for i := range addrs {
+				n := addrs[i].(*net.IPNet) // hacer type assertion
+				if n.IP.Equal(addr) {
+					skip = true
+				}
+			}
+			if !skip {
+				entries = append(entries, net.JoinHostPort(addr.String(),
+					fmt.Sprintf("%d", entry.Port)))
+			}
 		}
 		resChan <- entries
 	}(resChan)
