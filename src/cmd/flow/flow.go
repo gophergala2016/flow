bdad3ccaf6b50caaa3d5ec39a83282b17fc18b28
Arturo Vergara <hello@arturovm.me>
2016-01-23 18:10:40 -0600

    proof of concept

--- src/cmd/flow/flow.go
+++ src/cmd/flow/flow.go
@@ -1,3 +1,30 @@
 package main
 
-import "networking"
+import (
+	"log"
+	"networking"
+)
+
+func main() {
+	net := networking.Start()
+	cmd := networking.Command{
+		Cmd:  "print",
+		Args: map[string]string{"msg": "\nhola putos\n"},
+	}
+
+	for {
+		select {
+		case e := <-net:
+			netEvent(e)
+		case networking.In() <- cmd:
+		default:
+		}
+	}
+}
+
+func netEvent(event networking.Event) {
+	switch event.Type {
+	case networking.Connection:
+		log.Println("cliente conectado")
+	}
+}
--- src/networking/networking.go
+++ src/networking/networking.go
@@ -1,1 +1,64 @@
 package networking
+
+import "fmt"
+
+// EventType define la clase de eventos que se pueden emitir
+type EventType int
+
+const (
+	// Connection representa cliente conectado
+	Connection EventType = iota
+	// Disconnection representa cliente desconectado
+	Disconnection
+	// PeerLookup significa que el nodo está buscando peers
+	PeerLookup
+	// PeersFound significa que se encontraron peers
+	PeersFound
+	// Interp representa una solicitud de interpretación
+	Interp
+)
+
+// Event se utiliza para representar un evento emitido
+type Event struct {
+	Type EventType
+	Data interface{}
+}
+
+// Command se utiliza para mandar comandos a este módulo
+type Command struct {
+	Cmd  string
+	Args map[string]string
+}
+
+var in chan Command
+var out chan Event
+
+func init() {
+	in = make(chan Command)
+	out = make(chan Event)
+}
+
+// Start inicia el módulo
+func Start() <-chan Event {
+	go loop(in)
+	return out
+}
+
+// In regresa el channel para mandar comandos al módulo
+func In() chan<- Command {
+	return in
+}
+
+func loop(input <-chan Command) {
+	for c := range input {
+		switch c.Cmd {
+		case "print":
+			fmt.Println(c.Args["msg"])
+			out <- Event{
+				Type: Connection,
+				Data: nil,
+			}
+		default:
+		}
+	}
+}
